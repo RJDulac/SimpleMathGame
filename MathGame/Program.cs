a261e5cfedc6d3b05987e1cbeebe045435b9b252
Ryan Dulac <analyticrye@gmail.com>
2022-02-10 16:16:14 -0500

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace MathGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Q q = new Q(Q.random.Next(2) == 1);
+
+            while(true)
+            {
+                Console.WriteLine($"{q.N1} {q.Op} {q.N2} = ");
+                if(!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    return;
+                }
+                if(q.Check(i))
+                {
+                    Console.WriteLine("Right!");
+                    q = new Q(Q.random.Next(2) == 1);
+                }
+                else Console.WriteLine("Wrong Try again!");
+            }
+        }
+    }
+}
---
+++ MathGame/Q.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    public class Q
+    {
+        public static Random random = new Random();
+
+        public int N1 { get; private set; }
+        public int N2 { get; private set; }
+        public string Op { get; private set; }
+
+        public Q(bool add)
+        {
+            if (add) Op = "+";
+            else Op = "*";
+            N1 = random.Next(1, 10);
+            N2 = random.Next(1, 10);
+        }
+
+        public bool Check(int num)
+        {
+            if (Op == "+") return (num == N1 + N2);
+            else return num == N1 * N2;
+        }
+    }
+}
